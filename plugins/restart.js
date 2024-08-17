dcfbf8a5a54d4704d35eed4873d0cda1c290fd2a
sampathbo <106690470+sampathbo@users.noreply.github.com>
2024-08-17 11:10:21 +0530

    Add files via upload

--- plugins/restart.js
+++ plugins/restart.js
@@ -1,20 +1,21 @@
 const config = require('../config')
 const {cmd , commands} = require('../command')
+const {sleep} = require('../lib/functions')
 
 cmd({
-    pattern: "alive",
-    desc: "Check bot online or no.",
-    category: "main",
+    pattern: "restart",
+    desc: "restart the bot",
+    category: "owner",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
+const {exec} = require("child_process")
+reply("restarting...")
+await sleep(1500)
+exec("pm2 restart all")
 }catch(e){
 console.log(e)
 reply(`${e}`)
 }
 })
-
-
-
