643db9b9eaf54ba41b47d15def5bc5bc7a5b3c52
sampathbo <106690470+sampathbo@users.noreply.github.com>
2024-08-17 11:24:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://ibb.co/CtD22T8",
+ALIVE_MSG: process.env.ALIVR_MSG || "Hey,I'm Sampath-MD.I'm Alive Now!",
 };
